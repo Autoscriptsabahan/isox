425a90b23f8d8667e6e031bb29b72e35910a1396
linuxscript,net <33201553+Autoscriptsabahan@users.noreply.github.com>
2017-11-03 10:07:09 +0800

    Update dropmon.sh

--- dropmon.sh
+++ dropmon.sh
@@ -7,99 +7,68 @@ fi
 
 MYIP=$(wget -qO- ipv4.icanhazip.com)
 
-#vps="zvur";
-vps="aneka";
+echo ""| lolcat
+echo "|   Tgl-Jam    | PID   |   User Name  |      Dari IP      |"| boxes -d peek | lolcat
+echo "-------------------------------------------------------------"| lolcat
+data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
 
-if [[ $vps = "zvur" ]]; then
-	source="http://scripts.gapaiasa.com"
-else
-	source="https://raw.githubusercontent.com/r38865/VPS/master/Update"
-fi
-
-# go to root
-cd
-
-# check registered ip
-wget -q -O IP $source/IP.txt
-if ! grep -w -q $MYIP IP; then
-	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
-	if [[ $vps = "zvur" ]]; then
-		echo "Hubungi: Yuri Bhuana (fb.com/youree82 atau 0858 1500 2021)"
-	else
-		echo "Hubungi: Turut Dwi Hariyanto (fb.com/turut.dwi.hariyanto atau 085735313729)"
+echo "=================[ Checking Dropbear login ]================="| lolcat
+echo "-------------------------------------------------------------"| lolcat
+for PID in "${data[@]}"
+do
+	#echo "check $PID";
+	NUM=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+	USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk -F" " '{print $10}'`;
+	IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk -F" " '{print $12}'`;
+	waktu=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk -F" " '{print $1,$2,$3}'`;
+	if [ $NUM -eq 1 ]; then
+		echo "$waktu - $PID - $USER - $IP"| lolcat;
 	fi
-	rm -f /root/IP
+done
+
+
+echo "-------------------------------------------------------------"| lolcat
+data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
+
+echo "==================[ Checking OpenSSH login ]================="| lolcat
+echo "-------------------------------------------------------------"| lolcat
+for PID in "${data[@]}"
+do
+        #echo "check $PID";
+		NUM=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | wc -l`;
+		USER=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}'`;
+		IP=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $11}'`;
+		waktu=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $1,$2,$3}'`;
+        if [ $NUM -eq 1 ]; then
+                echo "$waktu - $PID - $USER - $IP"| lolcat;
+        fi
+done
+
+echo "-------------------------------------------------------------"| lolcat
+echo -e "==============[ User Monitor Dropbear & OpenSSH]=============" | lolcat
+PS3='Silahkan type nomor 1-3 lalu ENTER: '
+options=("Kick User" "Kembali Ke MENU" "Keluar")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Kick User")
+	read -p "Masukan angka (PID) user yang mau di Kick: " tendangan
+	kill -9 $tendangan
+	echo "Tunggu..." | lolcat
+	sleep 3
+	echo "Ok.. User Sudah Di Kick Boss.. !!" | lolcat
+	break
+	;;
+	"Kembali Ke MENU")
+	cd /root
+	menu
+	break
+	;;
+	"Keluar")
 	exit
-fi
-
-if [ $1 ]; then
-	port_dropbear=$1
-	log=/var/log/auth.log
-	loginsukses='Password auth succeeded'
-	echo ' '
-	echo ' '
-	echo "               Dropbear Users Login Monitor                    "
-	echo "---------------------------------------------------------------"
-	echo "    Date-time    |  PID      |  User Name      |  From Host    "
-	echo "---------------------------------------------------------------" 
-	pids=`ps ax |grep dropbear |grep  " $port_dropbear" |awk -F" " '{print $1}'`
-	for pid in $pids
-	do
-		pidlogs=`grep $pid $log |grep "$loginsukses" |awk -F" " '{print $3}'`
-		i=0
-		for pidend in $pidlogs
-		do
-			let i=i+1
-		done
-		
-		if [ $pidend ]; then
-			login=`grep $pid $log |grep "$pidend" |grep "$loginsukses"`
-			PID=$pid
-			user=`echo $login |awk -F" " '{print $10}' | sed -r "s/'/ /g"`
-			waktu=`echo $login |awk -F" " '{print $2,$3}'`
-			while [ ${#waktu} -lt 13 ]
-			do
-				waktu=$waktu" "
-			done
-			
-			while [ ${#user} -lt 16 ]
-			do
-				user=$user" "
-			done
-			
-			while [ ${#PID} -lt 8 ]
-			do
-				PID=$PID" "
-			done
-
-			fromip=`echo $login |awk -F" " '{print $12}' |awk -F":" '{print $1}'`
-			echo "  $waktu|  $PID | $user|  $fromip "
-		fi
-	done
+	break
+	;;
 	
-	echo "----------------------------------------------------------------"
-	echo " Ingin tendang user? Ketik kill -9 (angka PID)"
-	echo " Misal: kill -9 1234 [enter]"
-	echo "----------------------------------------------------------------"
-	
-	if [[ $vps = "zvur" ]]; then
-		echo " ALL SUPPORTED BY ZONA VPS UNTUK RAKYAT"
-		echo " https://www.facebook.com/groups/Zona.VPS.Untuk.Rakyat/"
-	else
-		echo " ALL SUPPORTED BY ANEKA VPS"
-		echo " https://www.facebook.com/aneka.vps.us"
-	fi
-	
-	echo "    DEVELOPED BY YURI BHUANA (fb.com/youree82, 085815002021)    "
-	echo "----------------------------------------------------------------"
-	echo ""
-else
-	echo "  Gunakan perintah ./dropmon [port]"
-	echo "  Contoh : ./dropmon 443"
-	echo ""
-fi
-
-exit 0
-
-cd ~/
-rm -f /root/IP
+*) echo invalid option;
+	esac
+done
